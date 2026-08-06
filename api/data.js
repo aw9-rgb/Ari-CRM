7b8a7d069463c026e6fb30c8024646faf8628e56
aw9-rgb <walfishari@gmail.com>
2026-08-05 21:24:01 -0400

    Implement GET and POST endpoints in data.js

---
+++ api/data.js
@@ -0,0 +1,25 @@
+const { getData, setData } = require('../lib/db');
+
+module.exports = async (req, res) => {
+  if (req.method === 'GET') {
+    const data = await getData();
+    res.status(200).json(data);
+    return;
+  }
+  if (req.method === 'POST') {
+    let body = req.body;
+    if (typeof body === 'string') {
+      try { body = JSON.parse(body); } catch (e) { body = {}; }
+    }
+    const current = await getData();
+    const next = {
+      contacts: body.contacts !== undefined ? body.contacts : current.contacts,
+      tasks: body.tasks !== undefined ? body.tasks : current.tasks,
+      history: body.history !== undefined ? body.history : current.history
+    };
+    await setData(next);
+    res.status(200).json({ ok: true });
+    return;
+  }
+  res.status(405).json({ error: 'method not allowed' });
+};
